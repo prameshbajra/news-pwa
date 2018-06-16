52849431e5844253e075056761f641d071f1daae
prameshbajra <pe.messh@gmail.com>
2018-06-16 15:49:45 +0530

    Ready to upload live

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(process.env.PORT || 3000);
